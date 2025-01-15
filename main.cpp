6ec5b6e0e2320a68ea13e5738e8f48d3a1b25015
DaveGZimori <85164510+DaveGZimori@users.noreply.github.com>
2025-01-15 17:39:17 +0800

    This is the main file for our programme.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    cout << "Welcome to the DZ Stock Exchange" << endl;
+    cout << "----------------------------------" << endl;
+    cout << "Select 1 to login" << endl;
+    cout << "Select 2 to register" << endl;
+    return 0;
+}
