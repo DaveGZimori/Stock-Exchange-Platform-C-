f92d0e2cfdd748dcd07fc15711d4c7e618c81507
DaveGZimori <85164510+DaveGZimori@users.noreply.github.com>
2025-01-15 18:02:34 +0800

    added the investor class

---
+++ investor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class investor
+{
+    private:
+        string name;
+        string password;
+        string email;
+        string account_number;
+        string phone_number;
+        double balance;
+};
