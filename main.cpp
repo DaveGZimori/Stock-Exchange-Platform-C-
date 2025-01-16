30913636f8a6fee3d361efb7d9733b8091eedd84
DaveGZimori <85164510+DaveGZimori@users.noreply.github.com>
2025-01-16 14:10:00 +0800

    created a login

--- main.cpp
+++ main.cpp
@@ -1,39 +1,143 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
 #include "investor.h"
 #include "account.h"
 
 using namespace std;
 
+string lgname;
+string lgpassword;
+bool login();
+void signup();
 
 void display_investor(investor A);
 void display_account(account Acc1);
 
+bool searchInCSV(const std::string &filename, const std::string &name, const std::string &password);
+
 int main() 
 {
+
     cout << "Welcome to the Uzumba Stock Exchange" << endl;
     cout << "----------------------------------" << endl;
     cout << "Select 1 to login" << endl;
     cout << "Select 2 to register" << endl;
 
-    investor A("King");
-    account Acc1;
-    A.set_account(&Acc1);
-    Acc1.set_owner(&A);
-    Acc1.set_account_number("0001");
-    Acc1.set_balance(250);
-    Acc1.get_owner()->set_password("072243");
-    Acc1.get_owner()->set_email("bg@gmail.com");
-    Acc1.get_owner()->set_phone_number("0778645238");
-    display_investor(A);
-    cout << "----------------------------------" << endl;
-    display_account(Acc1);
+    int choice;
+    cin >> choice;
+    if (choice == 1)
+    {
+        login();
+    }
+    else if (choice ==2)
+    {
+        signup();
+    }
+    else
+    {
+        cout << "Invalid choice" << endl;
+    }
+
+    if(login())
+    {
+        display_investor(investor(lgname));
+
+        cout << "----------------------------------" << endl;
+        cout << "Main menu" << endl;
+        cout << "Select 1 to view account details" << endl;
+        cout << "Select 2 to buy shares" << endl;
+        cout << "Select 3 to sell shares" << endl;
+        cout << "Select 4 to view transaction history" << endl;
+        cout << "Select 5 to view stock prices" << endl;
+        cout << "Select 6 to view stock market news" << endl;
+    }
     return 0;
+}
+
+
+bool login()
+{
+    bool log = false;
+    cout << "Enter your name: ";
+    cin >> lgname;
+    cout << "Enter your password: ";
+    cin >> lgpassword;
+    log = searchInCSV("Investors.csv", lgname, lgpassword);
+    if (log)
+    {
+        cout << "Login successful" << endl;
+    }
+    else
+    {
+        cout << "Login failed" << endl;
+    }
+    return log;
+}
+
+void signup()
+{
+    string name;
+    string password;
+    string email;
+    string phone_number;
+    cout << "Enter your name: "<<endl;
+    cin >> name;
+
+}
+
+bool searchInCSV(const string &filename, const string &name, const string &password) 
+{
+    std::ifstream file("Investors.csv");
+    std::string line;
+    bool found = false;
+    if (!file.is_open()) 
+    {
+        cerr << "Unable to open file: " << "Investors.csv" << endl;
+        return -1;
+    }
+
+    // Read the CSV file line by line
+    while (getline(file, line)) 
+    {
+        stringstream ss(line);
+        string value;
+        vector<string> row;
+
+        // Split the line into individual values
+        while (getline(ss, value, ',')) 
+        {
+            row.push_back(value);
+        }
+
+        // Search for the name in the current row
+        for (const auto &val : row) 
+        {
+            if (val == name) 
+            {
+                for (const auto &val : row) 
+                {
+                    if (val == password) 
+                    {
+                        found = true;
+                        break; // Exit the loop if found
+                    }
+                }
+            }
+        }
+
+        // Search for the password in the current row
+    }
+    file.close();
+    return found;
 }
 
 void display_investor(investor A)
 {
     cout << "Name: " << A.get_name() << endl;
-    cout << "Password: " << A.get_password() << endl;
+    //cout << "Password: " << A.get_password() << endl;
     cout << "Email: " << A.get_email() << endl;
     cout << "Phone number: " << A.get_phone_number() << endl;
     cout << "Account number: " << A.get_account()->get_account_number() << endl;
